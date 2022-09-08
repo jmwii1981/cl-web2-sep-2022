8d75c2024527d81103bf22426659c7dd34e396d7
Jan Michael Wallace <hello@janmichael.io>
2022-09-08 19:02:43 -0400

    first commit

---
+++ notes/2022-09-01-2022-lecture/index.js
@@ -0,0 +1,7 @@
+// alert(`Hello World!`); // Back tics can span multiple lines / can't be done with single or double quotes // also easier to use single and double quotes with.
+// in cl, only use let and const, never use var
+
+const x = 4
+const xIsFour = x == 4
+
+
